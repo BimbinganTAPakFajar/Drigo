f76068285f9c03b395202718de4c7dd187325c30
drigoalexander <drigosihombinga@gmail.com>
2023-07-24 09:30:47 +0700

    fix path to link form

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -143,7 +143,7 @@
             </p>
 
             <NuxtLink
-              to=""
+              to="https://docs.google.com/forms/d/e/1FAIpQLSfc68H0Tt_Q9uTyQzPIQnMM7mnIb9-mAQMfu3Y-bjtFQECdwA/viewform?usp=sf_link"
               class="inline-flex items-center px-5 py-2.5 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               Klik untuk menuju form
